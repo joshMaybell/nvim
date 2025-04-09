7a3d07a499f37ceadc6bb8d1ab7e30c3637ae819
Joshua Moler <josh@maybellquantum.com>
2025-04-09 11:45:46 -0600

    Make it work with file event observers

--- lua/scripts/set.lua
+++ lua/scripts/set.lua
@@ -14,6 +14,7 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
+vim.opt.writebackup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
@@ -38,3 +39,5 @@ vim.opt.splitright = true
 vim.g.coc_snippet_next = "<C-n>"
 vim.g.coc_snippet_prev = "<C-p>"
 vim.g.coc_snippet_active = 1
+
+vim.opt.ve = "onemore"
